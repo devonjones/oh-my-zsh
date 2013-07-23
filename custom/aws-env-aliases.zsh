db03dd44501bb8a7e887c409443158e2fd35969c
Devon Jones <devon@knewton.com>
2013-07-23 10:42:08 -0600

    added uat and analytics

--- custom/aws-env-aliases.zsh
+++ custom/aws-env-aliases.zsh
@@ -2,3 +2,5 @@
 alias saws='aws-env -e staging > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv'
 alias paws='aws-env -e production > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv'
 alias uaws='aws-env -e utility > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv'
+alias uataws='aws-env -e uat > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv'
+alias analaws='aws-env -e analytics > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv'
